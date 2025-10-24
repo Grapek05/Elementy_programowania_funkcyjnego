9ee500241fdf79b5288b5c889d0193c5a3945887
Grapek05 <tomgra1201@wp.pl>
2025-10-24 19:08:20 +0200

    Zadania z Haskella.

---
+++ 24.10.2025/suma.hs
@@ -0,0 +1,20 @@
+--  suma :: Int -> Int -> Int
+-- suma a b = a + b 
+
+suma :: (Num a) => a -> a -> a
+suma x y = x + y
+
+owoc = ["banan", "kiwi", "melon"]
+
+lotto = [1,30,31,40,42,49]
+
+temp = [16.5,17.0,(-13.5)]
+
+auto = ("Kia","Ceed",1.2,2023)
+v=3.14::Float
+
+l1 =[[1,2,3],[2,3],[4]]
+l2=[(1,2,3),(1,2,4),(8,9,(-1))]
+auta=[("X","Y",1.0,2022,"none"),("A","B",0.65,1990,"V")]
+costa1=((1,10),(12,3),(30,5))
+costa2=((1,"X"),("BMW",3,0),("kiwi","figa"))
