5ce7d66c7b6e013c8c77555d1a44b8c6cea73316
Grapek05 <tomgra1201@wp.pl>
2025-12-12 19:14:51 +0100

    Polecenia z Haskella na zajęciach.

---
+++ 12.12.2025/bmi.hs
@@ -0,0 +1,5 @@
+bmi w h
+    | w/h^2 <= 18.5 = "KFC & McDonald"
+    | w/h^2 <= 25.5 = "K. Figura"
+    | w/h^2 <= 30.0 = "STOP"
+    | otherwise = "Overkill"
---
+++ 12.12.2025/case.hs
@@ -0,0 +1,3 @@
+czyPrawda x = case x `mod` 2 of
+    0 -> "Prawda"
+    1 -> "Falsz"
